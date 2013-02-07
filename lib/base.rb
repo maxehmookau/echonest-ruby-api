9b704f130b01f14cb704bed5df26d5f26d340390
Max Woolf <maximus@zingysaturn.co.uk>
2013-02-07 19:07:49 +0000

    removes debug messages

--- lib/base.rb
+++ lib/base.rb
@@ -18,8 +18,6 @@ class Base
     options.each do |key, value|
       query_string << "#{ key }=#{ value }&"
     end
-    puts query_string
-    #response = HTTParty.get("#{ Base.base_uri }#{ options[:endpoint] }?api_key=#{ @api_key }&format=json&results=#{ options[:results] }&name=#{ @name }")
     response = HTTParty.get("#{ Base.base_uri }#{ endpoint }?api_key=#{ @api_key }&format=json&#{ query_string }")
 
     MultiJson.load(response.body, symbolize_keys: true)
