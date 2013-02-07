62c2754f8e507e2c38e6f30f45d0844f3f3a2b19
Max Woolf <maximus@zingysaturn.co.uk>
2013-02-07 19:06:47 +0000

    refactors more of the base connection get code

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,8 +18,8 @@ module Echonest
       @name
     end
 
-    def biographies(options = {})
-      response = get(endpoint: 'artist/biographies', results: options[:results])
+    def biographies(options = { results: 1 })
+      response = get('artist/biographies', results: options[:results], name: @name)
       biographies = []
       response[:response][:biographies].each do |b|
         biographies << Biography.new(text: b[:text], site: b[:site], url: b[:url])
--- lib/base.rb
+++ lib/base.rb
@@ -13,8 +13,15 @@ class Base
     4
   end
 
-  def get(options = {})
-    response = HTTParty.get("#{ Base.base_uri }#{ options[:endpoint] }?api_key=#{ @api_key }&format=json&results=#{ options[:results] }&name=#{ @name }")
+  def get(endpoint, options = {})
+    query_string = ""
+    options.each do |key, value|
+      query_string << "#{ key }=#{ value }&"
+    end
+    puts query_string
+    #response = HTTParty.get("#{ Base.base_uri }#{ options[:endpoint] }?api_key=#{ @api_key }&format=json&results=#{ options[:results] }&name=#{ @name }")
+    response = HTTParty.get("#{ Base.base_uri }#{ endpoint }?api_key=#{ @api_key }&format=json&#{ query_string }")
+
     MultiJson.load(response.body, symbolize_keys: true)
   end
 
