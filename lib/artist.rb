6c98b3755809e08ec262a2f11a53902ce29b8922
Max Woolf <maximus@zingysaturn.co.uk>
2013-02-08 18:58:32 +0000

    more dryness

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,8 +20,14 @@ module Echonest
       self.class.to_s.split('::').last.downcase
     end
 
+    def get_response(options = {})
+      calling_method = caller[0].split('`').last[0..-2]
+      get("#{ entity_name }/#{ calling_method }", options)
+    end
+
     def biographies(options = { results: 1 })
-      response = get("#{ entity_name }/#{ __method__ }", results: options[:results], name: @name)
+      #response = get("#{ entity_name }/#{ __method__ }", results: options[:results], name: @name)
+      response = get_response(results: options[:results], name: @name)
       biographies = []
       response[:response][:biographies].each do |b|
         biographies << Biography.new(text: b[:text], site: b[:site], url: b[:url])
@@ -46,6 +52,10 @@ module Echonest
     def hotttnesss
       response = get("#{ entity_name }/#{ __method__ }", name: @name)
       response[:response][:artist][:hotttnesss]
+    end
+
+    def images
+      response = get("#{ entity_name }/#{ __method__ }", name: @name)
     end
 
   end
