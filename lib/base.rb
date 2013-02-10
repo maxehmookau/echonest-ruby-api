84b5ec24cea3034092f88900fe296a258f37dce7
Max Woolf <maximus@zingysaturn.co.uk>
2013-02-10 11:32:19 +0000

    all kinds of sthings

--- lib/base.rb
+++ lib/base.rb
@@ -12,14 +12,33 @@ module Echonest
       @base_uri = "http://developer.echonest.com/api/v4/"
     end
 
+    # Gets the base URI for all API calls
+    #
+    # Returns a String
     def self.base_uri
       "http://developer.echonest.com/api/v#{ Base.version }/"
     end
 
+    # The current version of the Echonest API to be supported.
+    #
+    # Returns a Fixnum
     def self.version
       4
     end
 
+    # Performs a simple HTTP get on an API endpoint.
+    #
+    # Examples:
+    #     get('artist/biographies', results: 10)
+    #     #=> Array of Biography objects.
+    #
+    # Raises an +ArgumentError+ if the Echonest API responds with
+    # an error.
+    #
+    # * +endpoint+ - The name of an API endpoint as a String
+    # * +options+ - A Hash of options to pass to the end point.
+    #
+    # Returns a response as a Hash
     def get(endpoint, options = {})
       query_string = ""
       options.each do |key, value|
