0f823a77e0c62a1abed6920c65c8e9b6357ea24f
Seth Banks <stb@subimage.com>
2014-01-04 01:52:05 -0800

    FOR THE LOVE OF GOD NAMESPACE YOUR MODELS

--- spec/foreign_id_spec.rb
+++ spec/foreign_id_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe ForeignId do
+describe Echonest::ForeignId do
 
     it 'should parse the catalog id' do
-      foreign_id = ForeignId.new(catalog: "musicbrainz", foreign_id: "musicbrainz:artist:6fe07aa5-fec0-4eca-a456-f29bff451b04")
+      foreign_id = Echonest::ForeignId.new(catalog: "musicbrainz", foreign_id: "musicbrainz:artist:6fe07aa5-fec0-4eca-a456-f29bff451b04")
       foreign_id.catalog_id.should eq("6fe07aa5-fec0-4eca-a456-f29bff451b04")
     end
 
     it 'should parse an array of results' do
       results = [{catalog: "musicbrainz", foreign_id: "123"},{catalog: "musicbrainz", foreign_id: "456"}]
-      ForeignId.parse_array(results).count.should be 2
+      Echonest::ForeignId.parse_array(results).count.should be 2
     end
 
 end
